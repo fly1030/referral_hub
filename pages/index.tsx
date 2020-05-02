9f384843d28f3fc5be7ec8dd3c868834cdafca17
Fang Li <lifang_1030@hotmail.com>
2020-05-02 15:30:55 -0700

    connect to firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import MainHead from 'components/MainHead'
 import { Empty } from 'antd'
+import {loadDB} from '../lib/db.js';
+import { GetServerSideProps } from 'next';
 
-function Index() {
-	return (
-		<>
-			<MainHead title="Yes Onward" />
-			<Empty description="hello" />
-		</>
-	)
-}
+export const getServerSideProps: GetServerSideProps = async context => {
+	const firebase = loadDB();
+	const db = firebase.firestore();
+	const snapshot = await db.collection('users').get();
+	const data: Array<any> = [];
+	snapshot.forEach(doc => data.push(doc.data()));
+	// Pass data to the page via props
+	return { props: { data } }
+  }
+
+const Index = (props: {[key: string]: Array<any>}) => {
+	const {data} = props;
+	return <>
+		<MainHead title="Yes Onward" />
+		<Empty description="hello" />
+		<div>{data.map(user => user.name)}</div>
+	</>
+};
 
 export default Index
