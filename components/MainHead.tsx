b745522cddae6ef7ae9c5c45e25ad533d1c5e280
wahyudinata <wahyudinata@gmail.com>
2020-05-02 19:57:31 -0700

    logos and placement in index page

--- components/MainHead.tsx
+++ components/MainHead.tsx
@@ -10,7 +10,7 @@ export default function MainHead(props: Props) {
 			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
 			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.0/antd.min.css" />
 			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/basscss/8.1.0/css/basscss.min.css" />
-
+			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		</Head>
 	)
