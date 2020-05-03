13c207af5cd9e7c4fc9d77fb08419bae5fcc8c0a
wahyudinata <wahyudinata@gmail.com>
2020-05-02 18:20:28 -0700

    initial work on index page

--- components/MainHead.tsx
+++ components/MainHead.tsx
@@ -1,15 +1,17 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 type Props = {
-  title: string;
-};
+	title: string
+}
 export default function MainHead(props: Props) {
-  return (
-    <Head>
-      <title>{props.title}</title>
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.0/antd.min.css" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-  );
+	return (
+		<Head>
+			<title>{props.title}</title>
+			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.0/antd.min.css" />
+			<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/basscss/8.1.0/css/basscss.min.css" />
+
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		</Head>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,74 @@
 import MainHead from 'components/MainHead'
-import { Empty } from 'antd'
-import {loadDB} from '../lib/db.js';
-import { GetServerSideProps } from 'next';
+import { Divider, Card } from 'antd'
+import { AlertOutlined, CheckOutlined } from '@ant-design/icons'
 
-export const getServerSideProps: GetServerSideProps = async context => {
-	const firebase = loadDB();
-	const db = firebase.firestore();
-	const snapshot = await db.collection('users').get();
-	const data: Array<any> = [];
-	snapshot.forEach(doc => data.push(doc.data()));
+import { loadDB } from '../lib/db.js'
+import { GetServerSideProps } from 'next'
+import { ReactNode } from 'react'
+
+const style = {
+	entryButton: { border: '2px solid lightblue', borderRadius: 10 },
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const firebase = loadDB()
+	const db = firebase.firestore()
+	const snapshot = await db.collection('users').get()
+	const data: Array<any> = []
+	snapshot.forEach((doc) => data.push(doc.data()))
 	// Pass data to the page via props
 	return { props: { data } }
-  }
+}
 
-const Index = (props: {[key: string]: Array<any>}) => {
-	const {data} = props;
-	return <>
-		<MainHead title="Yes Onward" />
-		<Empty description="hello" />
-		<div>{data.map(user => user.name)}</div>
-	</>
-};
+function EntryButton(props: { title: string; subtitle: ReactNode; href: string }) {
+	return (
+		<a className="entryButton mr4" href={props.href}>
+			<Card hoverable style={style.entryButton}>
+				<h2 className="center">{props.title}</h2>
+			</Card>
+			<h3 className="mt1">{props.subtitle}</h3>
+		</a>
+	)
+}
+
+const Index = (props: { [key: string]: Array<any> }) => {
+	const { data } = props
+	return (
+		<>
+			<style jsx>{`
+				.entryButton {
+					display: block;
+				}
+				.entryButton:hover h3 {
+					font-weight: bold;
+				}
+			`}</style>
+
+			<MainHead title="Yes Onward" />
+			<div className="flex flex-row justify-center mt4">
+				<EntryButton
+					title="I can refer people"
+					subtitle={
+						<span>
+							Refer people without exposing your personal info <CheckOutlined className="h2" style={{ color: '#7cb305' }} />.
+						</span>
+					}
+					href="/login"
+				/>
+				<EntryButton
+					title="I want to be referred"
+					subtitle={
+						<span>
+							Get updates as the referral process progresses <AlertOutlined className="h2" style={{ color: '#f5222d' }} />.
+						</span>
+					}
+					href="/login"
+				/>
+			</div>
+			<Divider />
+			<div>{data.map((user) => user.name)}</div>
+		</>
+	)
+}
 
 export default Index
