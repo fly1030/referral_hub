60337f164bf4d642811230ef54dae75afbfc1ac2
wahyudinata <wahyudinata@gmail.com>
2020-05-10 14:19:25 -0700

    change referral

--- components/ReferrerInfoFormModal.tsx
+++ components/ReferrerInfoFormModal.tsx
@@ -1,84 +1,95 @@
-
-import { Modal, Button, Select, Form, Input} from 'antd';
-import { useState } from 'react';
+import { Modal, Button, Select, Form, Input } from 'antd'
+import { useState } from 'react'
 import { Companies } from 'lib/companies'
-import { loadDB } from 'lib/db';
-const { Option } = Select;
+import { loadDB } from 'lib/db'
+import router from 'next/router'
+const { Option } = Select
 
-function writeReferrerData(
-	currentComppany: string, 
-	companyEmail: string,
-) {
-	const firebase = loadDB();
-	const user = firebase.auth().currentUser;
+function writeReferrerData(currentComppany: string, companyEmail: string) {
+	const firebase = loadDB()
+	const user = firebase.auth().currentUser
 	if (user == null) {
-		console.log('no logged in user');
-		return;
+		console.log('no logged in user')
+		return
 	}
 
-    const userEmail = user.email;
-    if (userEmail == null) {
-		console.log('user has no proper email address');
-		return;
+	const userEmail = user.email
+	if (userEmail == null) {
+		console.log('user has no proper email address')
+		return
 	}
-	const firestore = firebase.firestore();
+	const firestore = firebase.firestore()
 	firestore.collection('referrers').doc(userEmail).set({
-	  company: currentComppany,
-	  loginEmail: userEmail,
-	  companyEmail: companyEmail,
-	});
-  }
+		company: currentComppany,
+		loginEmail: userEmail,
+		companyEmail: companyEmail,
+	})
+}
 
-function ReferrerInfoFormModal(props: {
-    visible: boolean, 
-    onCancel: () => void, 
-    onConfirm: () => void}) {
-    const [currentComppany, setCurrentCompany] = useState<string | null>(null);
-    const [companyEmail, setCompanyEmail] = useState<string | null>(null);
+function ReferrerInfoFormModal(props: { visible: boolean; onCancel: () => void; onConfirm: () => void }) {
+	const [currentComppany, setCurrentCompany] = useState<string | null>(null)
+	const [companyEmail, setCompanyEmail] = useState<string | null>(null)
 
-    const companyList = Companies.map(company => <Option value={company.name} key={company.name}>{company.name}</Option>);
-    companyList.push(<Option value='Others' key='Others'>{'Others'}</Option>);
-     
-    return (
-        <Modal
-            title="Tell us more about you"
-            visible={props.visible}
-            onOk={props.onConfirm}
-            onCancel={props.onCancel}
-            footer={[
-                <Button key="back" onClick={() => {
-                    if (currentComppany != null && companyEmail != null) {
-                        writeReferrerData(currentComppany, companyEmail);
-                    }
-					props.onConfirm();
-					window.location.href="/AvailableCases";
-                }}>
-                    OK
-                </Button>,
-            ]}>
-			<Form initialValues={{positions: '', resume: ''}} labelAlign="left">
-				<Form.Item label="Company" name="company" rules={[{required: true}]}>
-					<Select 
-						placeholder="Please select your current company" 
-						onChange={(value) => {setCurrentCompany(value)}} 
-						defaultValue={"Please select your current company"}
-						style={{ width: '100%' }}>
-						{companyList.map(company => company)}
+	const companyList = Companies.map((company) => (
+		<Option value={company.name} key={company.name}>
+			{company.name}
+		</Option>
+	))
+	companyList.push(
+		<Option value="Others" key="Others">
+			{'Others'}
+		</Option>,
+	)
+
+	return (
+		<Modal
+			title="Tell us more about you"
+			visible={props.visible}
+			onOk={props.onConfirm}
+			onCancel={props.onCancel}
+			footer={[
+				<Button
+					key="back"
+					onClick={() => {
+						if (currentComppany != null && companyEmail != null) {
+							writeReferrerData(currentComppany, companyEmail)
+						}
+						props.onConfirm()
+						router.push('/referrals')
+					}}
+				>
+					OK
+				</Button>,
+			]}
+		>
+			<Form initialValues={{ positions: '', resume: '' }} labelAlign="left">
+				<Form.Item label="Company" name="company" rules={[{ required: true }]}>
+					<Select
+						placeholder="Please select your current company"
+						onChange={(value) => {
+							setCurrentCompany(value)
+						}}
+						defaultValue={'Please select your current company'}
+						style={{ width: '100%' }}
+					>
+						{companyList.map((company) => company)}
 					</Select>
 				</Form.Item>
-				<Form.Item label="Company Email" name="companyEmail" rules={[{required: true}]}>
+				<Form.Item label="Company Email" name="companyEmail" rules={[{ required: true }]}>
 					<Input.Group compact>
-						<Input 
-						 style={{ width: '100%' }} 
-						 placeholder="Company Email Address.." 
-						 defaultValue=""
-						 onChange={(e) => {setCompanyEmail(e.target.value)}}
+						<Input
+							style={{ width: '100%' }}
+							placeholder="Company Email Address.."
+							defaultValue=""
+							onChange={(e) => {
+								setCompanyEmail(e.target.value)
+							}}
 						/>
 					</Input.Group>
 				</Form.Item>
 			</Form>
-        </Modal>
-    );
+		</Modal>
+	)
 }
 
 export default ReferrerInfoFormModal
--- pages/referrals.tsx
+++ pages/referrals.tsx
@@ -1,93 +1,90 @@
-import { Collapse, Button } from 'antd';
-import { GetServerSideProps } from 'next';
-import { loadDB } from 'lib/db';
-import { useState, useEffect } from 'react';
-import { User } from 'firebase';
-const { Panel } = Collapse;
+import { Collapse, Button } from 'antd'
+import { GetServerSideProps } from 'next'
+import { loadDB } from 'lib/db'
+import { useState, useEffect } from 'react'
+import { User } from 'firebase'
+const { Panel } = Collapse
 import MainHead from 'components/MainHead'
-import PageTopBar from 'components/PageTopBar';
-import { sendClaimedEmail } from 'pages/api/sendClaimedEmail';
+import PageTopBar from 'components/PageTopBar'
+import { sendClaimedEmail } from 'pages/api/sendClaimedEmail'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	const firebase = loadDB()
-    const db = firebase.firestore()
-    const referrers = await db.collection('referrers').get()
-   const referrersInfo: Array<{[key: string]: any}> = [] 
-    referrers.forEach((doc) => {
-        const referrerInfo = doc.data();
-        referrersInfo.push(referrerInfo);
-    })
+	const db = firebase.firestore()
+	const referrers = await db.collection('referrers').get()
+	const referrersInfo: Array<{ [key: string]: any }> = []
+	referrers.forEach((doc) => {
+		const referrerInfo = doc.data()
+		referrersInfo.push(referrerInfo)
+	})
 	const snapshot = await db.collection('cases').get()
-	const cases: Array<{[key: string]: any}> = []
-	snapshot.forEach((data) => { 
-        const casesFromCollection = data.data();
-       cases.push(casesFromCollection)
-    })
-	return { props: {cases, referrersInfo} }
+	const cases: Array<{ [key: string]: any }> = []
+	snapshot.forEach((data) => {
+		const casesFromCollection = data.data()
+		cases.push(casesFromCollection)
+	})
+	return { props: { cases, referrersInfo } }
 }
 
-async function onCaseClaimed(
-    availableCase: {[key: string]: any}, 
-    referrerEmail?: string | null,
-) {
-    if (referrerEmail == null) {
-        return
-    }
-	const firebase = loadDB();
-    const firestore = firebase.firestore();
-    const caseID = availableCase.caseID;
-    const caseDoc = await firestore.collection('cases').doc(caseID).get()
-    const caseDataFromDB = caseDoc.data()
-    const updatedData = {...caseDataFromDB, ...{
-        referrerEmail: referrerEmail,
-        caseStatus: 'Claimed',
-      }}
-    firestore.collection('cases').doc(caseID).set(updatedData);
-    sendClaimedEmail(availableCase, referrerEmail)
+async function onCaseClaimed(availableCase: { [key: string]: any }, referrerEmail?: string | null) {
+	if (referrerEmail == null) {
+		return
+	}
+	const firebase = loadDB()
+	const firestore = firebase.firestore()
+	const caseID = availableCase.caseID
+	const caseDoc = await firestore.collection('cases').doc(caseID).get()
+	const caseDataFromDB = caseDoc.data()
+	const updatedData = {
+		...caseDataFromDB,
+		...{
+			referrerEmail: referrerEmail,
+			caseStatus: 'Claimed',
+		},
+	}
+	firestore.collection('cases').doc(caseID).set(updatedData)
+	sendClaimedEmail(availableCase, referrerEmail)
 }
 
-function getExtra(
-    availableCase: {[key: string]: any}, 
-    referrerEmail?: string | null,
-) {
-    return  (
-        <Button 
-        type="primary" 
-        onClick={(event) => {
-            onCaseClaimed(availableCase, referrerEmail);
-            event.stopPropagation();
-        }}
-        disabled={availableCase.caseStatus !== 'Requested' || referrerEmail == null}
-        >
-            Claim
-        </Button>
-    )
+function getExtra(availableCase: { [key: string]: any }, referrerEmail?: string | null) {
+	return (
+		<Button
+			type="primary"
+			onClick={(event) => {
+				onCaseClaimed(availableCase, referrerEmail)
+				event.stopPropagation()
+			}}
+			disabled={availableCase.caseStatus !== 'Requested' || referrerEmail == null}
+		>
+			Claim
+		</Button>
+	)
 }
 
 function filterAvailableCasesByCompany(
-    cases: Array<{[key: string]: any}>, 
-    referrers: Array<{[key: string]: any}>, 
-    user: User | null,
-): Array<{[key: string]: any}> {
-    let targetedCompany: string | null = null
-    referrers.forEach((referrerInfo) => {
-        if (referrerInfo.loginEmail === user?.email) {
-            targetedCompany = referrerInfo.company;
-        }
-    })
-    const filteredCases: Array<{[key: string]: any}> = []
-    cases.forEach((caseInfo) => { 
-        if (caseInfo.company === targetedCompany && caseInfo.caseStatus === 'Requested') {
-            filteredCases.push(caseInfo)
-        }
-    })
-    return filteredCases;
+	cases: Array<{ [key: string]: any }>,
+	referrers: Array<{ [key: string]: any }>,
+	user: User | null,
+): Array<{ [key: string]: any }> {
+	let targetedCompany: string | null = null
+	referrers.forEach((referrerInfo) => {
+		if (referrerInfo.loginEmail === user?.email) {
+			targetedCompany = referrerInfo.company
+		}
+	})
+	const filteredCases: Array<{ [key: string]: any }> = []
+	cases.forEach((caseInfo) => {
+		if (caseInfo.company === targetedCompany && caseInfo.caseStatus === 'Requested') {
+			filteredCases.push(caseInfo)
+		}
+	})
+	return filteredCases
 }
 
-function AvailableCases(props: { [key: string]: Array<{[key: string]: any}> }) {
-    const [user, setUser] = useState<User | null>(null)
+function AvailableCases(props: { [key: string]: Array<{ [key: string]: any }> }) {
+	const [user, setUser] = useState<User | null>(null)
 
-    useEffect(() => {
+	useEffect(() => {
 		const firebase = loadDB()
 		firebase.auth().onAuthStateChanged((user) => {
 			if (user != null) {
@@ -98,55 +95,58 @@ function AvailableCases(props: { [key: string]: Array<{[key: string]: any}> }) {
 		})
 	}, [])
 
-    const avaliableCases = 
-        user == null ? 
-            [] : 
-            filterAvailableCasesByCompany(props.cases, props.referrersInfo, user);
-    
-    let currentReferrer: {[key: string]: any} | null = null
-    props.referrersInfo.forEach((referrerInfo) => {
-        if (referrerInfo.loginEmail === user?.email) {
-            currentReferrer = referrerInfo;
-        }
-    })
+	const avaliableCases = user == null ? [] : filterAvailableCasesByCompany(props.cases, props.referrersInfo, user)
 
-    if (currentReferrer == null) {
-        return <div/>;
-    }
+	let currentReferrer: { [key: string]: any } | null = null
+	props.referrersInfo.forEach((referrerInfo) => {
+		if (referrerInfo.loginEmail === user?.email) {
+			currentReferrer = referrerInfo
+		}
+	})
+
+	if (currentReferrer == null) {
+		return <div />
+	}
 
 	return (
 		<>
-            <MainHead title="Available Cases" />
-            <PageTopBar
+			<MainHead title="Available Cases" />
+			<PageTopBar
 				isLoggedIn={user != null}
 				onLogout={() => {
-					const firebase = loadDB();
-					firebase.auth().signOut();
-					setUser(null);
-					window.location.href="/";
+					const firebase = loadDB()
+					firebase.auth().signOut()
+					setUser(null)
+					window.location.href = '/'
 				}}
 				onLoginClicked={() => {}}
 			/>
-            <Collapse style={{margin: '20px'}}>
-                {
-                    avaliableCases.map((availableCase) => {
-                        return (
-                            <Panel 
-                                header={<b>{availableCase.candidateEmail}</b>} 
-                                key={availableCase.caseID} 
-                                extra={getExtra(availableCase, user?.email)}
-                            >
-                                <p>Candidate Email: <b>{availableCase.candidateEmail}</b></p>
-                                <p>Case Status: <b>{availableCase.caseStatus}</b></p>
-                                <p>Applying to: <b>{availableCase.company}</b></p>
-                                <p>Interested Positions: <b>{availableCase.positions}</b></p>
-                                <p>Additional Info: <b>{availableCase.comments}</b></p>
-                                <p>Case Created on: <b>{availableCase.createTime}</b></p>
-                            </Panel>)
-                    })
-                }
-
-            </Collapse>
+			<Collapse style={{ margin: '20px' }}>
+				{avaliableCases.map((availableCase) => {
+					return (
+						<Panel header={<b>{availableCase.candidateEmail}</b>} key={availableCase.caseID} extra={getExtra(availableCase, user?.email)}>
+							<p>
+								Candidate Email: <b>{availableCase.candidateEmail}</b>
+							</p>
+							<p>
+								Case Status: <b>{availableCase.caseStatus}</b>
+							</p>
+							<p>
+								Applying to: <b>{availableCase.company}</b>
+							</p>
+							<p>
+								Interested Positions: <b>{availableCase.positions}</b>
+							</p>
+							<p>
+								Additional Info: <b>{availableCase.comments}</b>
+							</p>
+							<p>
+								Case Created on: <b>{availableCase.createTime}</b>
+							</p>
+						</Panel>
+					)
+				})}
+			</Collapse>
 		</>
 	)
 }
