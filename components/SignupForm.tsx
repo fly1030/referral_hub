8bf48cbb82111e5497d326e5de3434a86f7df74a
wahyudinata <wahyudinata@gmail.com>
2020-05-10 13:09:00 -0700

    rework the login form styling

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,64 +1,69 @@
-import { Form, Input, Button, Divider } from 'antd';
-import {useState, ChangeEvent} from 'react';
+import { Form, Input, Button, Divider } from 'antd'
+import { useState, ChangeEvent } from 'react'
 import { loadDB } from '../lib/db.js'
-import { User } from 'firebase';
-
+import { User } from 'firebase'
+const layout = {
+	labelCol: { span: 8 },
+	wrapperCol: { span: 16 },
+}
 const tailLayout = {
-    wrapperCol: { offset: 4, span: 24 },
-};
-
-const style = {
-    confirmButton: { border: '2px solid lightblue', borderRadius: 10, width: '100%', maxWidth: 500},
-    emailInput: {marginLeft: 28, maxWidth: 385},
+	wrapperCol: { offset: 4, span: 24 },
 }
 
-const SignupForm = (props: {onSignupSuccess: (user: User | null) => void}) => {	
-    const onFinishFailed = () => {};
-    const [email, setEmail] = useState<string | null>(null);
-    const [password, setPassword] = useState<string | null>(null); 
-    
-    return (
-        <Form
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={() => {}}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <Input onChange={(event: ChangeEvent<HTMLInputElement>) => {setEmail(event.currentTarget.value)} }/>
-          </Form.Item>
-    
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password style={style.emailInput} onChange={(event: ChangeEvent<HTMLInputElement>) => {setPassword(event.currentTarget.value)} }/>
-          </Form.Item>
-    
-          <Form.Item {...tailLayout}>
-            <Button style={style.confirmButton} htmlType="submit" onClick={
-                () => {
-                    const firebase = loadDB();
-                    if (email == null || password == null) {
-                        return;
-                    }
-                    firebase.auth().createUserWithEmailAndPassword(email, password).then((u)=>{
-                        window.location.href="/";
-                    }).catch((error) => {
-                        console.log(error);
-                    });
-                }
-            }>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      );
+const style = {
+	confirmButton: { border: '2px solid lightblue', borderRadius: 10, width: '100%', maxWidth: 500 },
+	emailInput: { marginLeft: 28, maxWidth: 385 },
+}
+
+const SignupForm = (props: { onSignupSuccess: (user: User | null) => void }) => {
+	const onFinishFailed = () => {}
+	const [email, setEmail] = useState<string | null>(null)
+	const [password, setPassword] = useState<string | null>(null)
+
+	return (
+		<Form {...layout} name="basic" initialValues={{ remember: true }} onFinish={() => {}} onFinishFailed={onFinishFailed}>
+			<Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+				<Input
+					onChange={(event: ChangeEvent<HTMLInputElement>) => {
+						setEmail(event.currentTarget.value)
+					}}
+				/>
+			</Form.Item>
+
+			<Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+				<Input.Password
+					style={style.emailInput}
+					onChange={(event: ChangeEvent<HTMLInputElement>) => {
+						setPassword(event.currentTarget.value)
+					}}
+				/>
+			</Form.Item>
+
+			<Form.Item {...tailLayout}>
+				<Button
+					style={style.confirmButton}
+					htmlType="submit"
+					onClick={() => {
+						const firebase = loadDB()
+						if (email == null || password == null) {
+							return
+						}
+						firebase
+							.auth()
+							.createUserWithEmailAndPassword(email, password)
+							.then((u) => {
+								window.location.href = '/'
+							})
+							.catch((error) => {
+								console.log(error)
+							})
+					}}
+				>
+					Submit
+				</Button>
+			</Form.Item>
+		</Form>
+	)
 }
 
 export default SignupForm
--- components/login.tsx
+++ components/login.tsx
@@ -1,102 +1,98 @@
-import { Form, Input, Button, Divider } from 'antd';
-import {useState, ChangeEvent} from 'react';
+import { Form, Input, Button, Divider } from 'antd'
+import { useState, ChangeEvent } from 'react'
 import { loadDB } from '../lib/db.js'
-import { GoogleOutlined } from '@ant-design/icons';
-import { User } from 'firebase';
+import { GoogleOutlined } from '@ant-design/icons'
+import { User } from 'firebase'
 
 const layout = {
-    wrapperCol: { span: 24 },
-};
+	wrapperCol: { span: 24 },
+}
 const tailLayout = {
-    wrapperCol: { offset: 4, span: 24 },
-};
-
-const style = {
-  confirmButton: { border: '2px solid lightblue', borderRadius: 10, width: '100%', maxWidth: 500},
-  emailInput: {marginLeft: 28, maxWidth: 385},
+	wrapperCol: { offset: 4, span: 24 },
 }
 
-const Login = (props: {
-    onLoginSuccess: (user: User | null) => void
-}) => {	
-    const onFinishFailed = () => {};
-    const [email, setEmail] = useState<string | null>(null);
-    const [password, setPassword] = useState<string | null>(null); 
-    const {onLoginSuccess} = props;
-    
-    return (
-        <Form
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={() => {}}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item {...layout}>
-            <Button 
-              icon={<GoogleOutlined />}
-              type="primary" 
-              style={style.confirmButton} 
-              htmlType="submit" 
-              onClick={
-                () => {
-                  const firebase = loadDB();
-                  const provider = new firebase.auth.GoogleAuthProvider();
-                  firebase.auth().signInWithPopup(provider).then(
-                    result => {
-                      const user = result.user;
-                      onLoginSuccess(user);
-                    }
-                  ).catch(
-                    error => {
-                      const errorCode = error.code;
-                      console.log(errorCode);
-                      const errorMessage = error.message;
-                      console.log(errorMessage);
-                    }
-                  )
-                }
-            }>
-              Sign in with Google
-            </Button>
-          </Form.Item>
-          <Form.Item {...layout}>
-            <Divider />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <Input style={style.emailInput} onChange={(event: ChangeEvent<HTMLInputElement>) => {setEmail(event.currentTarget.value)} }/>
-          </Form.Item>
-    
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password onChange={(event: ChangeEvent<HTMLInputElement>) => {setPassword(event.currentTarget.value)} }/>
-          </Form.Item>
-    
-          <Form.Item {...tailLayout}>
-            <Button style={style.confirmButton} htmlType="submit" onClick={
-                () => {
-                    const firebase = loadDB();
-                    if (email == null || password == null) {
-                        return;
-                    }
-                    firebase.auth().signInWithEmailAndPassword(email, password).then((u)=>{
-                        window.location.href="/";
-                    }).catch((error) => {
-                        console.log(error);
-                    });
-                }
-            }>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      );
+const style = {
+	confirmButton: { border: '2px solid lightblue', borderRadius: 10, width: '100%', maxWidth: 500 },
+	emailInput: { marginLeft: 28, maxWidth: 385 },
+}
+
+const Login = (props: { onLoginSuccess: (user: User | null) => void }) => {
+	const onFinishFailed = () => {}
+	const [email, setEmail] = useState<string | null>(null)
+	const [password, setPassword] = useState<string | null>(null)
+	const { onLoginSuccess } = props
+
+	return (
+		<>
+			<Button
+				shape="circle"
+				icon={<GoogleOutlined />}
+				type="primary"
+				size="large"
+				htmlType="submit"
+				onClick={() => {
+					const firebase = loadDB()
+					const provider = new firebase.auth.GoogleAuthProvider()
+					firebase
+						.auth()
+						.signInWithPopup(provider)
+						.then((result) => {
+							const user = result.user
+							onLoginSuccess(user)
+						})
+						.catch((error) => {
+							const errorCode = error.code
+							console.log(errorCode)
+							const errorMessage = error.message
+							console.log(errorMessage)
+						})
+				}}
+			/>
+			<Divider>Or</Divider>
+			<Form name="basic" initialValues={{ remember: true }} onFinish={() => {}} onFinishFailed={onFinishFailed}>
+				<Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+					<Input
+						style={style.emailInput}
+						onChange={(event: ChangeEvent<HTMLInputElement>) => {
+							setEmail(event.currentTarget.value)
+						}}
+					/>
+				</Form.Item>
+
+				<Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+					<Input.Password
+						onChange={(event: ChangeEvent<HTMLInputElement>) => {
+							setPassword(event.currentTarget.value)
+						}}
+					/>
+				</Form.Item>
+
+				<Form.Item {...tailLayout}>
+					<Button
+						style={style.confirmButton}
+						htmlType="submit"
+						onClick={() => {
+							const firebase = loadDB()
+							if (email == null || password == null) {
+								return
+							}
+							firebase
+								.auth()
+								.signInWithEmailAndPassword(email, password)
+								.then((u) => {
+									window.location.href = '/'
+								})
+								.catch((error) => {
+									console.log(error)
+								})
+						}}
+					>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
+	)
 }
 
 export default Login
