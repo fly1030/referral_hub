cef68f544ad5a87946bcd52b41f3d18df8a77384
Fang Li <lifang_1030@hotmail.com>
2020-05-14 08:27:02 -0700

    narrower footer

--- components/IndexFooter.tsx
+++ components/IndexFooter.tsx
@@ -4,7 +4,7 @@ function PageFooter() {
     const tosLink = <a style = {{paddingLeft: 4, paddingRight: 4}} href="https://yesonward.com/privacy">Terms</a>
     const privacyLink = <a style = {{paddingLeft: 4, paddingRight: 4}} href="https://yesonward.com/privacy"> Privacy</a>
     return (
-        <div style={{padding: 40, backgroundColor: '#363636', display: 'flex', justifyContent: 'space-between'}}>
+        <div style={{padding: 30, backgroundColor: '#363636', display: 'flex', justifyContent: 'space-between'}}>
             <PageLogo textStyle = {{ cursor: 'pointer', paddingLeft: 8, fontSize: 25,  color: '#FFFFFF'}}/>
             <div style={{
                 color: '#FFFFFF',
