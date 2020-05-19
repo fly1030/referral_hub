177fcdffaa1a0114260c859385ab95f04dbb5d2d
Fang Li <lifang_1030@hotmail.com>
2020-05-19 16:24:58 -0700

    add rivian

--- lib/companies.ts
+++ lib/companies.ts
@@ -15,6 +15,7 @@ export const Companies = [
 	{ key: 'instacart', name: 'Instacart', careerPage: 'https://instacart.careers/current-openings/'},
 	{ key: 'uber', name: 'Uber', careerPage: 'https://www.uber.com/us/en/careers/list/' },
 	{ key: 'hulu', name: 'Hulu', careerPage: 'https://careers.hulu.com/search-jobs' },
+	{ key: 'rivian', name: 'Rivian', careerPage: 'https://rivian.com/careers/' },
 ]
 export type Companies = typeof Companies
 export type Company = Companies[number]
