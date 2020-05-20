f74092968b49cf95dccff184f831503232eb0576
Fang Li <lifang_1030@hotmail.com>
2020-05-20 15:10:04 -0700

    add IBM

--- lib/companies.ts
+++ lib/companies.ts
@@ -15,6 +15,7 @@ export const Companies = [
 	{ key: 'instacart', name: 'Instacart', careerPage: 'https://instacart.careers/current-openings/'},
 	{ key: 'uber', name: 'Uber', careerPage: 'https://www.uber.com/us/en/careers/list/' },
 	{ key: 'hulu', name: 'Hulu', careerPage: 'https://careers.hulu.com/search-jobs' },
+	{ key: 'ibm', name: 'Ibm', careerPage: 'https://careers.ibm.com/'},
 	{ key: 'rivian', name: 'Rivian', careerPage: 'https://rivian.com/careers/' },
 ]
 export type Companies = typeof Companies
